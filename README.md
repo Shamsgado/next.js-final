1feb1e5d8453723deac61e88cc81bcdc3f5225fe
Shamsu Gado <39752630+Shamsgado@users.noreply.github.com>
2025-08-11 20:32:10 -0400

    Final

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 ## Next.js App Router Course - Starter
 
 This is the starter template for the Next.js App Router Course. It contains the starting code for the dashboard application.
 
 For more information, see the [course curriculum](https://nextjs.org/learn) on the Next.js Website.
-=======
-# next.js-final
->>>>>>> c7055e0d662d2312644666da507a093a64feb575
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,14 +1,13 @@
-
 'use client';
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import clsx from 'clsx';
-
+  import Link from 'next/link';
+  import { usePathname } from 'next/navigation';
+  import clsx from 'clsx';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -37,8 +36,7 @@ export default function NavLinks() {
               {
                 'bg-sky-100 text-blue-600': pathname === link.href,
               },
-            )}
-          >
+            )}          >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
           </Link>
